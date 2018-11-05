e4a78d8f980d8fdbdd5db41055c44bc294914703
Bruno Andreetto <bruno.andreetto@usp.br>
2018-11-05 16:36:57 -0200

    Add better example

--- WallExtrusion/src/Scene.tsx
+++ WallExtrusion/src/Scene.tsx
@@ -4,6 +4,36 @@ import * as THREE from 'three'
 type Point = [number, number]
 type Line = [Point, Point]
 
+const wallMatrixExample: Line[] = [
+    [[313, 124],[521, 124]],
+    [[356, 370],[421, 370]],
+    [[93, 119],[227, 119]],
+    [[269, 253],[348, 253]],
+    [[385, 120],[521, 120]],
+    [[208, 212],[285, 213]],
+    [[516, 334],[516, 219]],
+    [[93, 161],[93, 123]],
+    [[211, 370],[297, 370]],
+    [[270, 366],[270, 255]],
+    [[382, 373],[382, 251]],
+    [[521, 300],[521, 217]],
+    [[479, 372],[520, 373]],
+    [[207, 210],[207, 126]],
+    [[366, 213],[421, 213]],
+    [[370, 215],[370, 119]],
+    [[319, 215],[319, 119]],
+    [[450, 254],[512, 254]],
+    [[92, 371],[93, 311]],
+    [[516, 125],[517, 159]],
+    [[520, 372],[520, 301]],
+    [[99, 123],[211, 125]],
+    [[419, 179],[419, 127]],
+    [[95, 279],[95, 219]],
+    [[91, 223],[91, 249]],
+    [[100, 369],[153, 370]],
+    [[313, 120],[384, 119]]
+]
+
 class Scene extends React.Component {
     mount: any
     scene: THREE.Scene
@@ -51,14 +81,14 @@ class Scene extends React.Component {
         return [(x1 + x2)/2, (y1 + y2)/2]
     }
 
-    createGeometryFromPoints(pointsArray: Line[], scaling = .25, plant = new THREE.Geometry) {
+    createGeometryFromPoints(pointsArray: Line[], scaling = .01, plant = new THREE.Geometry) {
         if (pointsArray.length <= 0) {
             return plant;
         } else {
             const [firstLine, ...rest] = pointsArray
             const d = this.distanceBetweenPoints(...firstLine)
             const angle = this.angleFromXAxis(firstLine) 
-            const wall2 = new THREE.BoxGeometry(d*scaling, this.wallWidth*scaling, this.wallHeigth*scaling).rotateZ(angle)
+            const wall2 = new THREE.BoxGeometry(d*scaling, this.wallWidth*scaling*50, this.wallHeigth*scaling*50).rotateZ(angle)
             
             const [x, y] = this.getLineMidPoint(firstLine)
             plant.merge(wall2, this.createTranslationMatrix(x*scaling, y*scaling, 0))
@@ -79,14 +109,10 @@ class Scene extends React.Component {
             2000
         )
         const renderer = new THREE.WebGLRenderer({ antialias: true })
-        const geometry = this.createGeometryFromPoints([
-            [[0, 0], [0, 1]], 
-            [[0, 2], [0, 3]],
-            [[0, 3], [4, 5]],
-            [[4, 5], [4, 2]],
-            [[4, 1], [4, 0]],
-            [[4, 0], [0, 0]],
-        ])
+        const geometry = this.createGeometryFromPoints(wallMatrixExample)
+            .applyMatrix(this.createTranslationMatrix(-3.2, -2.5, -1))
+            .rotateX(-Math.PI/6)
+            .rotateX(Math.PI)
         const material = new THREE.MeshBasicMaterial({ color: 0xff00ff })
         const cube = new THREE.Mesh(geometry, material)
 
@@ -126,12 +152,12 @@ class Scene extends React.Component {
     }
 
     animate() {
-        this.cube.rotation.x += 0.004
-        this.cube.rotation.y += 0.0045
-        this.cube.rotation.z += 0.005
-        this.edges.rotation.x += 0.004
-        this.edges.rotation.y += 0.0045
-        this.edges.rotation.z += 0.005
+        // this.cube.rotation.x += 0.004
+        // this.cube.rotation.y += 0.0045
+        // this.cube.rotation.z += 0.005
+        // this.edges.rotation.x += 0.004
+        // this.edges.rotation.y += 0.0045
+        // this.edges.rotation.z += 0.005
 
         this.renderScene()
         this.frameId = window.requestAnimationFrame(this.animate)
