4b27505e7eece2ef84f3f239c8108a9c04bbf730
Daniel <daniel@livup.com.br>
2018-11-11 20:45:26 -0200

    New features android and ios
    This reverts commit ebeaad6d918c418662500430d13cd0d17dec4f1b, reversing
    changes made to 9ae282dc8628a584e269915865a3d19e3fb6c746.

--- android/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./gradlew ${1:-installDevMinSdkDevKernelDebug} --stacktrace && adb shell am start -n br.poli.interativo/host.exp.exponent.MainActivity
--- ios/build/Intermediates.noindex/wallup.build/Debug-iphonesimulator/wallup.build/Script-AA38385B7D8D0854176B72FC.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-wallup/Pods-wallup-resources.sh"
-
